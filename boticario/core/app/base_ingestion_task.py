074eb33b20344c9b4b1210f76e6e1e7b4d4772c0
Thiago da Cunha Borges <thcborges@gmail.com>
2023-03-16 14:29:18 +0000

    BaseIngestionTask reads its files from defined path

--- boticario/core/app/base_ingestion_task.py
+++ boticario/core/app/base_ingestion_task.py
@@ -1,9 +1,46 @@
+from pathlib import Path
+
+from pandas import DataFrame, concat
+
 from boticario.core.app.task import Task
+from boticario.core.helpers.file_reader import FileReader
+from boticario.core.helpers.path import (
+    DATA_PATH,
+    is_processed_path,
+    set_processed_folder,
+)
 from boticario.logger import get_logging
 
 logging = get_logging(__name__)
 
 
 class BaseIngestionTask(Task):
+    __data_path = DATA_PATH / 'raw' / 'base'
+    __processed_path = set_processed_folder(__data_path)
+
+    def __is_file_to_process(self, file: Path) -> bool:
+        return file.is_file() and not is_processed_path(file)
+
+    def __list_files(self):
+        logging.info('Listing base files to process')
+        return [
+            file
+            for file in self.__data_path.glob('**/*')
+            if self.__is_file_to_process(file)
+        ]
+
+    def __read_files(self, files: list[Path]) -> DataFrame:
+        logging.info('Reading base files')
+        data_frames = []
+        for file in files:
+            reader = FileReader(file).get_reader()
+            data_frame = reader.read_to_pandas()
+            data_frame['filename'] = file.name
+            data_frames.append(data_frame)
+        return concat(data_frames)
+
     def run(self):
         logging.info('Base Ingestion')
+        files = self.__list_files()
+        logging.debug('Files to process: %s', str(files))
+        data_frame = self.__read_files(files)
