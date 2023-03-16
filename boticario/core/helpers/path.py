bf817db7bff14138fbcfd31a222f7560d21a771b
Thiago da Cunha Borges <thcborges@gmail.com>
2023-03-16 13:43:19 +0000

    Adding features to path library to read and process base data

--- boticario/core/helpers/path.py
+++ boticario/core/helpers/path.py
@@ -1,6 +1,8 @@
 from datetime import date
 from pathlib import Path
 
+PROCESSED_FOLDER = '.processed'
+
 
 def get_root_path() -> Path:
     """Returns the root path to the package
@@ -8,7 +10,7 @@ def get_root_path() -> Path:
     Returns:
         Path: root path to the package
     """
-    return Path(__file__).parent.parent.parent
+    return Path(__file__).parents[2]
 
 
 def get_config_file(config: str) -> Path:
@@ -30,7 +32,13 @@ def get_logging_path() -> Path:
     return logging_path
 
 
-def get_data_path() -> Path:
-    root = get_root_path()
-    data_path = root / 'data'
-    return data_path
+def set_processed_folder(path: Path | str) -> Path:
+    return Path(path) / PROCESSED_FOLDER
+
+
+def is_processed_path(path: Path | str) -> bool:
+    return PROCESSED_FOLDER in Path(path).parts
+
+
+ROOT_PATH = get_root_path()
+DATA_PATH = ROOT_PATH / 'data'
