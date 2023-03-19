c87e3e704f9f0a042aa0902ef5642a4880791e20
Thiago da Cunha Borges <thcborges@gmail.com>
2023-03-19 03:06:27 +0000

    Adding cli to run all base's tasks

--- boticario/cli.py
+++ boticario/cli.py
@@ -1,6 +1,15 @@
+import traceback
+
 import click
 
-from boticario import BaseIngestionTask
+from boticario import (
+    BaseIngestionTask,
+    BaseTable1Task,
+    BaseTable2Task,
+    BaseTable3Task,
+    BaseTable4Task,
+)
+from boticario.core.services.spotify_api import SpotifyAPI
 from boticario.logger import config_logger, get_logging
 
 logging = get_logging(__name__)
@@ -14,4 +23,86 @@ def main():
 @main.command()
 def base_ingestion_task():
     logging.info('base ingestion task')
-    BaseIngestionTask().run()
+    try:
+        BaseIngestionTask().run()
+    except BaseException as error:
+        logging.error(str(error))
+        logging.error(traceback.format_exc())
+
+
+@main.command()
+def base_create_table1_task():
+    logging.info('base create table 1')
+    try:
+        BaseTable1Task().run()
+    except BaseException as error:
+        logging.error(str(error))
+        logging.error(traceback.format_exc())
+
+
+@main.command()
+def base_create_table2_task():
+    logging.info('base create table 2')
+    try:
+        BaseTable2Task().run()
+    except BaseException as error:
+        logging.error(str(error))
+        logging.error(traceback.format_exc())
+
+
+@main.command()
+def base_create_table3_task():
+    logging.info('base create table 3')
+    try:
+        BaseTable3Task().run()
+    except BaseException as error:
+        logging.error(str(error))
+        logging.error(traceback.format_exc())
+
+
+@main.command()
+def base_create_table4_task():
+    logging.info('base create table 4')
+    try:
+        BaseTable4Task().run()
+    except BaseException as error:
+        logging.error(str(error))
+        logging.error(traceback.format_exc())
+
+
+@main.command
+def base_task_group():
+    logging.info('base ingestion task')
+    try:
+        BaseIngestionTask().run()
+    except BaseException as error:
+        logging.error(str(error))
+        logging.error(traceback.format_exc())
+
+    logging.info('base create table 1')
+    try:
+        BaseTable1Task().run()
+    except BaseException as error:
+        logging.error(str(error))
+        logging.error(traceback.format_exc())
+
+    logging.info('base create table 2')
+    try:
+        BaseTable2Task().run()
+    except BaseException as error:
+        logging.error(str(error))
+        logging.error(traceback.format_exc())
+
+    logging.info('base create table 3')
+    try:
+        BaseTable3Task().run()
+    except BaseException as error:
+        logging.error(str(error))
+        logging.error(traceback.format_exc())
+
+    logging.info('base create table 4')
+    try:
+        BaseTable4Task().run()
+    except BaseException as error:
+        logging.error(str(error))
+        logging.error(traceback.format_exc())
